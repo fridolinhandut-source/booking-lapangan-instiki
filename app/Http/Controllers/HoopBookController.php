fce5f1977bbace37b2ffd1f308f7a7b2577dfa47
Fridolin Handut <fridolinhandut@gmail.com>
2026-06-25 19:58:46 -0700

    Add all routes, controllers, and views

--- app/Http/Controllers/HoopBookController.php
+++ app/Http/Controllers/HoopBookController.php
@@ -3,67 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Field;
-use App\Models\User;
 
 class HoopBookController extends Controller
 {
-    // Cari Lapangan
     public function cari()
     {
-        $lapangan = Field::all();
-        return view('cari', compact('lapangan'));
+        return view('cari-lapangan');
     }
 
-    // Booking Saya
     public function bookingSaya()
     {
-        return view('pesan'); // Menggunakan view pesan untuk daftar booking
+        return view('booking-saya');
     }
 
-    // Kalender
     public function kalender()
     {
         return view('kalender');
     }
 
-    // Pembayaran
     public function pembayaran()
     {
         return view('pembayaran');
     }
 
-    // Promo
     public function promo()
     {
         return view('promo');
     }
 
-    // Favorit
     public function favorit()
     {
         return view('favorit');
     }
 
-    // Ulasan
     public function ulasan()
     {
         return view('ulasan');
     }
 
-    // Pesan
     public function pesan()
     {
         return view('pesan');
     }
 
-    // Pengaturan
     public function pengaturan()
     {
         return view('pengaturan');
     }
 
-    // Bantuan
     public function bantuan()
     {
         return view('bantuan');
