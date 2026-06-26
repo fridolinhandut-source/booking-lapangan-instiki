41c7877abce34555b55520d20204453c5b97a38a
Fridolin Handut <fridolinhandut@gmail.com>
2026-06-25 18:21:37 -0700

    Fix: redirect root to login page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\HoopBookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
